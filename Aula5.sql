120857507a8cc1e7c39ea0dccbb15d1c9486f290
talisma <62837677+talisma-cassoma@users.noreply.github.com>
2021-01-06 23:16:01 +0100

    Delete Aula5.sql

--- Aula5.sql
+++
@@ -1,8 +0,0 @@
-/*para afixar a bd usamos o comando select * from pessoas */
-use cadastro;
-/* vamos cadastrar alguem no nosso db*/
-insert into pessoas 
-(nome, nascimento,sexo, peso, altura ,nacionalidade)
-value
-('Maria','1984-12-30','F','55.2','1.60','Angola');
-select * from pessoas;
